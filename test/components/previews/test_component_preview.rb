878a516b0e844a0ef770ad608c4d0a0293c33960
Jon Palmer <jon@alignable.com>
2019-12-25 10:02:25 -0500

    Upgrade AV:C

--- test/components/previews/test_component_preview.rb
+++ test/components/previews/test_component_preview.rb
@@ -1,9 +1,9 @@
 class TestComponentPreview < ActionView::Component::Preview
   def with_default_title
-    render(TestComponent, title: "Test component default") { "Default Title"}
+    render(TestComponent, title: "Test component default") { "Default Title" }
   end
 
   def with_long_title
-    render(TestComponent, title: "This is a really long title to see how the component renders this") { "Long Title"}
+    render(TestComponent, title: "This is a really long title to see how the component renders this") { "Long Title" }
   end
 end
